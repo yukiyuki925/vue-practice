52df91f9392664c07bedd775584acc776719fc78
yukiyuki925 <yuki.work1999@gmail.com>
2024-03-21 09:15:09 +0900

    入力欄の文字を受け渡す

--- my-vite-todo/src/components/MainTodo.vue
+++ my-vite-todo/src/components/MainTodo.vue
@@ -1,7 +1,24 @@
+<script setup>
+import { ref } from "vue";
+const todoRef = ref("");
+const todoListRef = ref([]);
+const addTodo = () => {
+  const id = new Date().getTime();
+  todoListRef.value.push({ id: id, task: todoRef.value });
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+  todoRef.value = "";
+};
+</script>
+
 <template>
   <div class="box_input">
-    <input type="text" class="todo_input" placeholder="+ TODOを入力" />
-    <button class="button">追加</button>
+    <input
+      type="text"
+      class="todo_input"
+      v-model="todoRef"
+      placeholder="+ TODOを入力"
+    />
+    <button class="button" @click="addTodo">追加</button>
   </div>
 </template>
 
