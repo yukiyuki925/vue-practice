5c8f7d6f63b21c50799a82d9d5ecb53f9d5d7055
yukiyuki925 <yuki.work1999@gmail.com>
2024-03-23 19:31:05 +0900

    router実装

--- my-vite-todo/src/router/index.js
+++ my-vite-todo/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainTodo from "/src/pages/MainTodo.vue";
+import About from "/src/pages/About.vue";
 
 const routes = [
   {
     path: "/",
     name: "Top",
     component: MainTodo,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
   },
 ];
 
