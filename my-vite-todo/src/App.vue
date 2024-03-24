fb1fc62c3861c930cca760b5cd431f4f2c3d8bca
yukiyuki925 <yuki.work1999@gmail.com>
2024-03-24 13:55:38 +0900

    blog修正

--- my-vite-todo/src/App.vue
+++ my-vite-todo/src/App.vue
@@ -4,11 +4,10 @@ import TheFooter from "./components/TheFooter.vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
 const goBlog = () => {
   router.push("/blog");
 };
-
-router.push({ name: "BlogId", params: { id: 1 } });
 </script>
 
 <template>
@@ -19,8 +18,13 @@ router.push({ name: "BlogId", params: { id: 1 } });
       | <router-link to="/about">About</router-link> |
       <span @click="goBlog">Blog</span>
       |
-      <router-link to="{ name: 'BlogId', params: {id: 1}}">Blog1</router-link> |
-      <router-link to="{ name: 'BlogId', params: {id: 2}}">Blog2</router-link>
+      <router-link :to="{ name: 'BlogId', params: { id: 1 } }"
+        >Blog1</router-link
+      >
+      |
+      <router-link :to="{ name: 'BlogId', params: { id: 2 } }"
+        >Blog2</router-link
+      >
     </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
